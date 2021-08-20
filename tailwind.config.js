01e993a124cee762ce51252c7a2178df1c43a60b
Tyler D <tyler@tdaigle.com>
2021-08-20 15:36:29 -0400

    added projects and images

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
+// set NODE_ENV to production to purge
 module.exports = {
   purge: ["./public/*.html", "./src/*.css"],
   darkMode: false, // or 'media' or 'class'
