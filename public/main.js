473b08771886f8b6917046a62d070130d01ac4c4
Tyler D <tyler@tdaigle.com>
2021-12-01 11:08:47 -0500

    add and removed some projects

--- public/main.js
+++ public/main.js
@@ -21,24 +21,3 @@ document.getElementById("submit-button").addEventListener("click", (e) => {
     status.innerHTML = "";
   }, 3000);
 });
-
-// The Halloween Countdown is hosted on Heroku which means that it takes some time to
-// wake up. If it is in a sleeping state when trying to load the image for the countdown,
-// it just won't load the image at all. This function just fetches the svg data from
-// the heroku server and then replaces the loading message with the actual svg.
-
-window.addEventListener("load", async () => {
-  console.log("Page Loaded...");
-  // make sure Heroku is actually awake.
-  const data = await fetch("https://halloween-countdown.herokuapp.com/");
-  if (data.ok) {
-    const svgData = await data.text();
-    console.log(svgData);
-    document.getElementById("halloween-container").innerHTML = svgData;
-    console.log(data);
-  } else {
-    document.getElementById("halloween-container").innerHTML = `
-      <p>There appears to be an error with Heroku... but trust me this thing does work. 😉</p>
-    `;
-  }
-});
